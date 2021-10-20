46281ac1580a9c5057beee3b47d4d2ef97b9bf18
Ayushdsk <deshmukhayush2000@gmail.com>
2021-10-20 22:31:23 +0530

    done

---
+++ planning.js
@@ -0,0 +1,25 @@
+//BOOK MY SHOW PLANNING
+
+// Carousal planning
+   /* We need Reusable carousal
+     1. Hero Carousal
+     2. Common Carousal
+     3. Cast Carousal
+   */
+
+//Pages
+   /*
+     Home Page
+     Movie Pages
+     Category Pages
+     Authenticatoin Page
+     Razorpay payment gateway
+   */
+
+//Building Plan
+  /*
+    layout
+    Routes
+    Pages
+    Coponents
+  */
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        ayush:
+    {
+      50: '#fdfce0',
+      100: '#f5f3b9',
+      200: '#eeeb90',
+      300: '#e7e265',
+      400: '#e0da3b',
+      500: '#c6c122',
+      600: '#9a9618',
+      700: '#6e6b0f',
+      800: '#424005',
+      900: '#171500',
+    },
+
+    navCol: {
+
+{
+
+  {
+    50: '#eaf3fe',
+    100: '#cdd7e6',
+    200: '#2B3148',
+    300: '#90a2bc',
+    400: '#7087a8',
+    500: '#576e8f',
+    600: '#43556f',
+    700: '#2f3d50',
+    800: '#1a2532',
+    900: '#030d17',
+  }
+}
+    }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
